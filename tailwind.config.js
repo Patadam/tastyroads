9be88596d0d50ad3be424cd38a7d0f834d2280f0
Patrick <patadamrich@gmail.com>
2021-12-02 11:08:53 +1000

    feat: added hero section

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      brightness: {
+        25: ".25",
+        175: "1.75",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
